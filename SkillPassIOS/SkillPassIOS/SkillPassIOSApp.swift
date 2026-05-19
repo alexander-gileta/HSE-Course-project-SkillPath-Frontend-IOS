070c647c551561f7dbc312b1639ff277854f97e3
alexander-gileta <alexander.gileta@yandex.ru>
2026-05-19 21:17:05 +0100

    Add files via upload

---
+++ SkillPassIOS/SkillPassIOS/SkillPassIOSApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct SkillPassIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .ignoresSafeArea()
+        }
+    }
+}
